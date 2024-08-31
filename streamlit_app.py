66716c60b69712d3ccdc26e1203b8d330710ba03
Aadish Verma <842starlite@gmail.com>
2024-08-31 19:26:27 +0000

    Added things

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,11 @@
 import streamlit as st
 import prep, save as save
 
+st.session_state.messages = [{
+        "role": "assistant",
+        "content": "Welcome to DoveAI! What seems to be your problem today?"
+}]
+st.session_state.solved = False 
 pages = {
     "Dole of Doves": [
         st.Page(prep.prep_screen, title="Prepare for a meeting"),
