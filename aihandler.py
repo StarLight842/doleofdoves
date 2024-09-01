839117d9d8435ffee511f6c6970ceddc8662954b
Aadish Verma <842starlite@gmail.com>
2024-09-01 04:46:39 +0000

    DoveAI 2.0

--- aihandler.py
+++ aihandler.py
@@ -16,30 +16,19 @@ model_name="gemini-1.5-flash",
 generation_config=generation_config,
 # safety_settings = Adjust safety settings
 # See https://ai.google.dev/gemini-api/docs/safety-settings
-system_instruction="You are a helpful tech support agent for Adobe Connect users. Below are the dialog options, along with IDs, that you can return:\nMIC: No one can hear the client\nDISPL: The client can't see anyone\nCAM: No one can see the client\nSND: Client can't hear anything\nINT: Client is having internet problem (e.g. video freezing)\nHave a short conversation (1-4) message exchanged with the client to pinpoint the issue they are experiencing, then send a final message that only contains the ID of the correct dialog to show.\n",
+system_instruction="You are a helpful tech support agent for Adobe Connect users. Below are the dialog options, along with IDs, that you can return:\nMIC: No one can hear the client\nDISPL: The client can't see anyone\nCAM: No one can see the client\nSND: Client can't hear anything\nINT: Client is having internet problem (e.g. video freezing)\nELSE: Another, distinct issue\nHave a short conversation (1-4) message exchanged with the client to pinpoint the issue they are experiencing, then send a final message that only contains the ID of the correct dialog to show.\n",
 )
 
-def get_history(st_hist):
-    history = []
-    role_map = {"user": "user", "assistant": "model"}
-    for x in st_hist:
-        item = {"role": role_map[x["role"]], "parts": [x["content"]]}
-        history.append(item)
-    return history   
-
-def get_response(input, st_hist):
-    chat_session = model.start_chat(history=get_history(st_hist))
-    response = chat_session.send_message(input)
-    return response.text 
-
-responses = {
+dialogs = {
 "MIC": """
 **If a ~~tree falls~~ you talk and no one hears it, did it really happen?**
 It looks like there’s an issue with your microphone. If no one can hear you, make sure you have selected the right microphone and try a few different ones in our microphone testing tool.
 """,
 "DISPL": """
 **Hmmmm. Try glasses?**
-If you can’t see anyone, make sure your display is set to the one you’re currently looking at (e.g. the one you’re filling out this form on). If you’re sure that you’re looking at the right display, try signing out and signing back in, or using the desktop version (or browser version if you’re using the desktop one).
+If you can’t see anyone, make sure your display is set to the one you’re currently looking at (e.g. the one you’re filling out this form on). If you’re sure that you’re looking at the right display, try signing out and signing back in, or using the desktop version (or vice versa if you’re using the desktop one).
+
+Download the desktop version of AC [here](https://helpx.adobe.com/adobe-connect/connect-downloads-updates.html)
 """,
 "CAM": """
 **If no one can see you, it’s a ‘them’ problem.**
@@ -54,11 +43,44 @@ If you can’t hear anything, the most likely problem is that your speaker is mu
 It appears your bandwidth is not up to par. (You can confirm this by checking our internet testing tool; we recommand 30-50 mbps or higher). The fastest hack to save on bandwidth is to select the three dots on your camera pod in AC then click “Pause incoming video.” This will freeze the video of all other participants (including your instructor), and should greatly reduce your network usage.
  For the long term, talk to your parents or guardians about a faster wifi plan or using wired internet (Ethernet); if those options aren’t possible, try switching to the desktop version of Adobe Connect, which is less network-demanding. 
 """,
+"ELSE": """
+**Oops, you've exhausted our knowledge.**
+It appears your problem is so rare (or we were so forgetful) that we didn't add a tool for it here. 
+
+Fill out the [OHS tech support form](https://ohs.stanford.edu/techsupport) to get help from the professionals.
+"""
 }
 links = {
 "MIC": ["microphone", "test-your-microphone"],
-"DISPL": ["", "https://helpx.adobe.com/adobe-connect/connect-downloads-updates.html"],
 "CAM": ["camera", "test-your-camera"],
 "SND": ["speakers", "test-your-speakers"],
 "INT": ["internet", "test-your-internet"],
 }
+
+class Conversation:
+    def __init__(self):
+        self.messages = [{
+            "role": "assistant",
+            "content": "Welcome to DoveAI! What seems to be your problem today?"
+        }]
+        self.solved_id = ""
+        self.session = model.start_chat()
+    def update(self, p):
+        # get ai response
+        response = self.session.send_message(p).text
+        # check for solved
+        for k in dialogs.keys():
+            if k in response:
+                self.solved_id = k
+        # add to messages
+        self.messages.append({"role": "user", "content": p})
+        if self.solved_id == "":
+            self.messages.append({"role": "assistant", "content": response})
+    def get_ai_dialog(self):
+        dialog = "✅ Problem solved by DoveAI"
+        dialog += "\n\n" + dialogs[self.solved_id]
+        if self.solved_id not in ["DISPL", "ELSE"]:
+            dialog += "\n\n Try our simple " + links[self.solved_id][0] + " testing tool [here](https://doleofdoves.streamlit.app/#"+links[self.solved_id][1]+")"
+        if self.solved_id not in ["ELSE"]:
+            dialog += "\n\n Still having issues? Fill out an [OHS tech support form](https://ohs.stanford.edu/techsupport) to get help from the professionals"
+        return dialog
--- save.py
+++ save.py
@@ -1,52 +1,21 @@
 import streamlit as st
-import aihandler
-if "messages" not in st.session_state:
-    st.session_state.messages = [{
-            "role": "assistant",
-            "content": "Welcome to DoveAI! What seems to be your problem today?"
-    }]
-if "solved" not in st.session_state:
-    st.session_state.solved = False 
+from aihandler import Conversation
 
-def save_screen():   
-    title = "# We'll save you from ~~certain death~~ a functional absence"
-    st.markdown(f"{title}", unsafe_allow_html=True)
-    # start chat
-    if st.button("Restart chat"):
-        st.session_state.messages = [{
-            "role": "assistant",
-            "content": "Welcome to DoveAI! What seems to be your problem today?"
-        }]
-        st.session_state.solved = False  
+
+def save_screen():
+    st.markdown("# We'll save you from ~~certain death~~ a functional absence")
+
+    if "ai_state" not in st.session_state or st.button("Restart chat"):
+        st.session_state["ai_state"] = Conversation()  
     # Display chat messages from history on app rerun
-    for message in st.session_state.messages:
-        with st.chat_message(message["role"]):
-            st.markdown(message["content"])
-
+    if st.session_state["ai_state"].solved_id != "":
+        st.markdown(st.session_state["ai_state"].get_ai_dialog())
+    else:
+        for message in st.session_state["ai_state"].messages:
+            with st.chat_message(message["role"]):
+                st.markdown(message["content"])
     # React to user input
     if prompt := st.chat_input("Type here..."):
-        if not st.session_state.solved:
-        
-            # Display user message in chat message container
-            with st.chat_message("user"):
-                st.markdown(prompt)
-            # Add user message to chat history
-            st.session_state.messages.append({"role": "user", "content": prompt})
-
-            response = aihandler.get_response(prompt, st.session_state.messages)
-            for k in aihandler.responses.keys():
-                if k in response:
-                    response = aihandler.responses[k]
-                    st.session_state.solved = True
-                    st.session_state.id = k
-                    break
-            
-            with st.chat_message("assistant"):
-                st.markdown(response)
-                if st.session_state.solved:
-                    if st.session_state.id == "DISPL":
-                        st.markdown("Download the desktop version of AC [here]("+aihandler.links['DISPL'][1]+")")
-                    else:
-                        st.markdown("Try our simple " + aihandler.links[st.session_state.id][0] + " testing tool [here](https://doleofdoves.streamlit.app/#"+aihandler.links[st.session_state.id][1]+")")
-            st.session_state.messages.append({"role": "assistant", "content": response})
-            print(st.session_state.messages)
+        if st.session_state["ai_state"].solved_id == "":
+            st.session_state["ai_state"].update(prompt)
+            st.rerun()
