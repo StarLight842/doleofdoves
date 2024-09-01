280f9710c2e847fe806d31eccde219c21d732d1c
Aadish Verma <842starlite@gmail.com>
2024-09-01 00:55:49 +0000

    Mic and cam tools fully working

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,40 +1,17 @@
-# import streamlit as st
-# import prep, save as save
-
-# st.session_state.messages = [{
-#         "role": "assistant",
-#         "content": "Welcome to DoveAI! What seems to be your problem today?"
-# }]
-# st.session_state.solved = False 
-# pages = {
-#     "Dole of Doves": [
-#         st.Page(prep.prep_screen, title="Prepare for a meeting"),
-#         st.Page(save.save_screen, title="SAVE ME I'M DYING"),
-#     ]
-# }
-# st.caption("TEAM DOLE OF DOVES: LABOR DAY HACKATHON SUBMISSION")
-# pg = st.navigation(pages)
-# pg.run()
-import av
-import numpy as np
-import pydub
 import streamlit as st
-from streamlit_webrtc import WebRtcMode, webrtc_streamer
+import prep, save as save
 
-if "x" not in st.session_state:
-    st.session_state.x = 0
-
-def process_audio(frame: av.AudioFrame) -> av.AudioFrame:
-    raw_samples = frame.to_ndarray()
-    st.write("count", np.size(raw_samples))
-    return av.AudioFrame()
-
-
-webrtc_streamer(
-    key="audio-filter",
-    mode=WebRtcMode.SENDRECV,
-    audio_frame_callback=process_audio,
-    async_processing=True,
-    rtc_configuration={"iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]},
-)
-st.write("x = ", st.session_state.x)
+st.session_state.messages = [{
+        "role": "assistant",
+        "content": "Welcome to DoveAI! What seems to be your problem today?"
+}]
+st.session_state.solved = False 
+pages = {
+    "Dole of Doves": [
+        st.Page(prep.prep_screen, title="Prepare for a meeting"),
+        st.Page(save.save_screen, title="SAVE ME I'M DYING"),
+    ]
+}
+st.caption("TEAM DOLE OF DOVES: LABOR DAY HACKATHON SUBMISSION")
+pg = st.navigation(pages)
+pg.run()
--- tools.py
+++ tools.py
@@ -1,5 +1,7 @@
 import streamlit as st
-
+from streamlit_webrtc import webrtc_streamer, AudioProcessorBase, WebRtcMode
+import time
+import numpy as np
 #
 # SPEAKER
 #
@@ -9,44 +11,41 @@ def speaker_tool():
 #
 # MICROPHONE
 #
+import streamlit as st
 def mic_tool():
-    st.write("microphone "*100)
+    st.write("Click 'Start' to show a bar with the current volume level of your incoming mic (e.g. it should increase if you do something loud for a few seconds e.g. scream). It will be a little janky due to software limitations. If the volume isn't updating or is different than what you expect, try the 'Select device' menu and go through the options available.")
 
+    webrtc_ctx = webrtc_streamer(
+        key="speech-to-text",
+        mode=WebRtcMode.SENDONLY,
+        audio_receiver_size=1024,
+        rtc_configuration={"iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]},
+        media_stream_constraints={"video": False, "audio": True},
+    )
+
+    status_indicator = st.empty()
+
+    if not webrtc_ctx.state.playing:
+        return
+
+    status_indicator.write("Loading...")
+
+    while 1==1:
+        if webrtc_ctx.audio_receiver:
+            try:
+                audio_frames = webrtc_ctx.audio_receiver.get_frames(timeout=1)
+                volumes = []
+                for f in audio_frames:
+                    volumes.append(np.average(f.to_ndarray()))
+                with status_indicator:
+                    volume = int(np.average(volumes))
+                    st.slider("Volume input level", min_value=-50, max_value=200, value=volume)
+            except:
+                pass
+        time.sleep(0.05)
 #
 # CAMERA
 #
-from streamlit_webrtc import webrtc_streamer, WebRtcMode
-import numpy as np
-import av
-def camera_tool():
-    st.session_state.x = 0
-    st.write("Click 'Start' to see a streamed output from your default camera. If the output isn't showing or is lower quality than you expect, try the 'Select device' menu (which also shows a preview of camera output) and go through the options available.")
-    webrtc_ctx = webrtc_streamer(
-        key="object-detection",
-        mode=WebRtcMode.SENDRECV,
-        rtc_configuration={"iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]},
-        media_stream_constraints={"video": True, "audio": False},
-        async_processing=True,
-        audio_frame_callback=process_audio,
-    )
-    st.write("count = ", st.session_state.x)
-
-def process_audio(frame: av.AudioFrame) -> av.AudioFrame:
-    raw_samples = frame.to_ndarray()
-    st.session_state.x = np.size(raw_samples)
-    return frame
-
-
-
-#
-# INTERNET
-#
-def bandwidth_tool():
-    st.write("internet "*100)
-
-
-"""
-from streamlit_webrtc import webrtc_streamer, WebRtcMode
 def camera_tool():
     st.write("Click 'Start' to see a streamed output from your default camera. If the output isn't showing or is lower quality than you expect, try the 'Select device' menu (which also shows a preview of camera output) and go through the options available.")
     webrtc_ctx = webrtc_streamer(
@@ -56,4 +55,9 @@ def camera_tool():
         media_stream_constraints={"video": True, "audio": False},
         async_processing=True
     )
-"""
+
+#
+# INTERNET
+#
+def bandwidth_tool():
+    st.write("internet "*100)
